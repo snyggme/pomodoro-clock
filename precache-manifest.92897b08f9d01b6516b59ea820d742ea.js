7e5f34339d600dc86d41b87044fa45f19cfe753f
snyggme <rcbabahin@gmail.com>
2018-10-06 11:44:05 +0300

    Updates

---
+++ precache-manifest.92897b08f9d01b6516b59ea820d742ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f41d346ce9ebc121a229",
+    "url": "/pomodoro-clock/static/js/runtime~main.f41d346c.js"
+  },
+  {
+    "revision": "15795e392af06be73452",
+    "url": "/pomodoro-clock/static/js/main.15795e39.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/pomodoro-clock/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "15795e392af06be73452",
+    "url": "/pomodoro-clock/static/css/main.e40f663f.chunk.css"
+  },
+  {
+    "revision": "ddf6501b81e6310c9d30703e28c6aee4",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
